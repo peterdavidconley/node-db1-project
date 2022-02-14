acb85baa586b94695a5dcc444e54d12cb476cb42
Peter Conley <peterdavidconley@gmail.com>
2022-02-14 17:31:47 -0500

    wrote db function for getById

--- api/accounts/accounts-model.js
+++ api/accounts/accounts-model.js
@@ -5,7 +5,7 @@ const getAll = () => {
 }
 
 const getById = id => {
-  return db('accounts').where({ id: id })
+  return db('accounts').where('id', id).first()
 }
 
 async function create ({ name, budget}) {
